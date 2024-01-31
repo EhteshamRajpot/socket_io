b974d8fe583766de71d02df1a8c0dfa2bf623a69
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-02-01 00:36:06 +0500

    first commit

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ io.on("connection", (socket) => {
     io.emit("getUsers", users);
   });
 });
-
+ 
 server.listen(process.env.PORT || 4000, () => {
   console.log(`server is running on port ${process.env.PORT || 4000}`);
 });
